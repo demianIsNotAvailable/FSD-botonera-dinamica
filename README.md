43cec2d48bc2c0e2e134c993b41be4b7e628ba63
Demian Ortizlanzas <d.ortizlanzas@geekshubs.com>
2023-09-29 13:46:22 +0200

    feat: reset button

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Ejemplo de botonera generada dinámicamente con tantos botones como el usuario indique por prompt (NO USAR PROMPT EN EL MUNDO REAL, POR FAVOR).
-Cada botón se genera aleatoriamente on/off, y como demostración de interacción, pulsar un botón setea aleatoriamente los demás.
+Cada botón se genera aleatoriamente on/off, y como demostración de interacción, pulsar un botón setea aleatoriamente los demás. Los botones usan atributos "onclick" como demostración, y el botón de reset que reinicia el programa usa event listeners.
--- js/main.js
+++ js/main.js
@@ -3,51 +3,76 @@ const randomBool = () => {
     return Math.random() < 0.5
 }
 
-
 // CREAR DINÁMICAMENTE CON ARRAY AUXILIAR EN CÓDIGO
 
-// definimos un array vacío
-let buttons = []
 
-// pedimos una cantidad y rellenamos el array con tantos booleanos aleatorios como nos den
-const createButtons = () => {
-    let amount = prompt('Elige un número natural entre 3 y 10. No trolees, que no he validado el input.')
-    for (let i=0; i< amount; i++) {
-        buttons[i] = randomBool()
-    }
+// Definimos un array vacío y pedimos una cantidad y rellenamos el array con tantos booleanos aleatorios como nos den
+// let buttons = []
+// const createButtons = () => {
+//     let amount = prompt('Elige un número natural entre 3 y 10. No trolees, que no he validado el input.')
+//     for (let i=0; i< amount; i++) {
+//         buttons[i] = randomBool()
+//     }
 
 // iteramos sobre el array, creando un botón para cada elemento que tendrá id = button_X siendo X la posición que ocupa en el array
-// y definiendo su propiedad 'checked' según el valor booleano que corresponda. Esta propiedad en HTML SOLO DEFINE EL ESTADO INICIAL Y NO ES INTERACTIVA
-    buttons.forEach((ischecked, position) => {
-        document.querySelector('.board').innerHTML += `<label class="toggle">
-        <input type="checkbox" class="toggle-input" id="button_${position}" onclick="shuffleButtons(${position}, ${amount})" ${ischecked ? 'checked' : ''}>
-        <span class="toggle-slider"></span>
-    </label>`
-    })
-}
-createButtons()
+// y definiendo su propiedad 'checked' según el valor booleano que corresponda. 
+// Esta propiedad en HTML SOLO DEFINE EL ESTADO INICIAL Y NO ES INTERACTIVA
+//     buttons.forEach((ischecked, position) => {
+//         document.querySelector('.boardContainer').innerHTML += `<label class="toggle">
+//         <input type="checkbox" class="toggle-input" id="button_${position}" onclick="shuffleButtons(${position}, ${amount})" ${ischecked ? 'checked' : ''}>
+//         <span class="toggle-slider"></span>
+//     </label>`
+//     })
+//     document.getElementById('resetButton').addEventListener('click', resetButtons)
+
+// }
+// const resetButtons = () => {
+//     buttonsToReset = document.querySelectorAll('.toggle')
+//     buttonsToReset.forEach(button => {
+//         if (button.classList.contains('toggle')) {
+//             button.remove()}
+//     })
+//     buttons = []
+//     createButtons()
+// }
+
+// createButtons()
 
 
 // CREAR DINÁMICAMENTE A PELO
 
-//// Misma idea que antes, pero ahora iteramos directamente con un bucle "for" tantas veces como la cantidad pedida
-// const createButtonsNoArray = () => {
-//     let amount = prompt('no vaciles, mete un número natural')
-//     for (let position=0; position<amount; position++) {
-//         const isThisButtonChecked = randomBool()
+// Misma idea que antes, pero ahora iteramos directamente con un bucle "for" tantas veces como la cantidad pedida
 
-//         document.querySelector('.board).innerHTML += `<label class="toggle">
-//         <input type="checkbox" class="toggle-input" id="button_${position}" onclick="shuffleButtons(${position}, ${amount})" ${isThisButtonChecked ? 'checked' : ''}>
-//         <span class="toggle-slider"></span>
-//         </label>`
-//     }
-// }
-// createButtonsNoArray()
+const createButtonsNoArray = () => {
+    let amount = prompt('no vaciles, mete un número natural')
+    for (let position=0; position<amount; position++) {
+        const isThisButtonChecked = randomBool()
+
+        document.querySelector('.boardContainer').innerHTML += `<label class="toggle">
+        <input type="checkbox" class="toggle-input" id="button_${position}" onclick="shuffleButtons(${position}, ${amount})" ${isThisButtonChecked ? 'checked' : ''}>
+        <span class="toggle-slider"></span>
+        </label>`
+    }
+    document.getElementById('resetButton').addEventListener('click', resetButtonsNoArray)
+}
+
+const resetButtonsNoArray = () => {
+    buttonsToReset = document.querySelectorAll('.toggle')
+    buttonsToReset.forEach(button => {
+        if (button.classList.contains('toggle')) {
+            button.remove()}
+    })
+    createButtonsNoArray()
+}
+
+
+createButtonsNoArray()
 
 
 // Esta función es llamada cada vez que apretamos un botón. Recibe como argumentos el botón pulsado y la cantidad de botones, e itera sobre todos los botones.
 // Para los que sean DISTINTOS del que hemos pulsado, asigna un booleano aleatorio a su estado dinámico .checked. Si la usáramos con la versión con array, deberíamos
 // ampliarla para que primero actualizara los valores del array, y después actualizara el DOM basándose en el array. 
+
 const shuffleButtons = (pressedButton, amount) => {
     for (let button=0; button<amount; button++) {
         if (button !== pressedButton) {
